5cb681b5068b3ba6df9879f1b5d031a800a28690
D-Eva-Code <anagwonyeadaeze42@gmail.com>
2025-08-27 20:46:41 +0100

    added and updated myapp.js

---
+++ myApp.js
@@ -0,0 +1,20 @@
+app.get('/api/:date?',(req,res)=>{
+    let datestring= req.params.date;
+    let time;
+    if(!isNaN (Number(datestring) )){
+        time= new Date(parseInt(datestring));
+        }
+    else if(!datestring){
+        time=new Date();
+       
+    }
+    else{
+        time= new Date(datestring);
+        
+    }
+    if(isNaN(time.getTime())){
+         return res.json({"error":"\"invalid Date\""});
+    }
+    return res.json({"unix": time.getTime(), "utc": time.toUTCString() });
+    
+});
